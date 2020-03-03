e0845f96cb99ff444b2c11bad7c8f77c65daedd6
akniles <alexslinka98@gmail.com>
2020-03-03 19:39:29 +0300

    .

--- algorithms/Treap.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Admin on 19.11.2019.
-//
-
-#include "Treap.h"
--- algorithms/zen.cpp
+++
@@ -1,8 +0,0 @@
-#include <Treap.cpp>
-
-int main() {
-    Treap<int> treap;
-    treap.Insert(1, 1);
-    treap.Insert(1, 2);
-    return 0;
-}
